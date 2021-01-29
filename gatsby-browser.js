54e1e101ee9d634379149ba6c7f4aa90dfa80424
Kamil Rusiecki <kamil.rusiecki@gmail.com>
2021-01-29 12:18:49 +0100

    Initial commit

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,11 @@
  *
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
-
+import './src/styles/global.css'
+import "@fontsource/raleway/100.css"
+import "@fontsource/raleway/200.css"
+import "@fontsource/raleway/300.css"
+import "@fontsource/raleway/400.css"
+import "@fontsource/raleway/500.css"
+import "@fontsource/raleway/700.css"
 // You can delete this file if you're not using it
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        raleway: ["raleway", "sans-serif"]
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
